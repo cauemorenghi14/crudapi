59d5a9111d3a131e478db7d7a3bb91f263b7ba5c
caue <cauemorenghi14@gmail.com>
2024-06-25 15:38:39 -0300

    feat: implementando middleware auth para funções de listagem e criação de usuário

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -4,6 +4,8 @@ Route.group(() => {
     Route.resource('users', 'UsersController')
         .apiOnly()
         .middleware({
+            index: ['auth'],
+            store: ['auth'],
             update: ['auth'],
             destroy: ['auth'],
         })
